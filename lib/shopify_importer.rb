1fe0729d34311095ba76958151ee9c324658ed69
Warren Sangster <warren@moneyspyder.co.uk>
2016-01-01 22:12:09 +0000

    HH-103

--- lib/shopify_importer.rb
+++ lib/shopify_importer.rb
@@ -1,9 +1,11 @@
 class ShopifyImporter
 
   MAPPING = {
+    "Handle" => :slug,
     "Title" => :name,
     "SEO Description" => :meta_description,
-    "Variant Price" => :price
+    "Variant Price" => :price,
+    "Body (HTML)" => :description
   }
   
 
@@ -12,7 +14,14 @@ class ShopifyImporter
       shopify_hash = Hash[*row.headers.zip(row.fields).flatten]
       spree_hash = Hash[*MAPPING.collect { |key,value| [value, shopify_hash[key]] }.flatten]
       spree_hash[:shipping_category_id] = Spree::ShippingCategory.first.id
-      Spree::Product.create(spree_hash)
+
+      unless Spree::Product.where(slug: spree_hash[:slug]).exists?
+        Spree::Product.create(spree_hash)
+      else
+        product = Spree::Product.find_by_slug(spree_hash[:slug])
+        product.description = spree_hash[:description]
+        product.save
+      end
     end
   end
 
--- spec/shopify_importer_spec.rb
+++ spec/shopify_importer_spec.rb
@@ -26,6 +26,48 @@ describe ShopifyImporter do
 
     end
 
+    product_description = '<p>These stunning clear opal vases were designed to represent the 7 days of the week</p>'
+
+    describe %Q{file contains handle "7-days-vases" with description "#{product_description}"} do
+
+      context 'product exists with a slug "7-days-vases" but without a description' do
+
+        let(:file) do
+          File.open(
+            File.join('spec', 'data', 'products_export.csv')
+          )
+        end
+
+        let(:default_shipping_category) do
+          Spree::ShippingCategory.create(:name => 'Default Shipping')
+        end
+
+        let(:product) do
+          default_attrs = {
+            :available_on => Time.zone.now,
+            :shipping_category => default_shipping_category,
+            :price => 14.99,
+            :name => '7 Days Vases',
+            :slug => '7-days-vases'
+          }
+          result = Spree::Product.create!(default_attrs)
+          result.shipping_category = default_shipping_category
+          result.save!
+          result
+        end
+
+        it %Q{changes the description to "#{product_description}"} do
+          expect{
+            ShopifyImporter.import file
+          }.to change{
+            Spree::Product.find(product).description
+          }.from(nil).to(product_description)
+        end
+
+      end      
+
+    end
+
   end
 
 end
